75b8273be6f084b8d0408bae56386a78e0df93a1
Duc Dao Minh <brokenheart1712@gmail.com>
2019-02-16 11:01:40 +0700

    Fix PostList File

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-	posts = [
-		{title: 'First Post', content: 'This is the first post\' content'},
-		{title: 'Second Post', content: 'This is the second post\' content'},
-		{title: 'Third Post', content: 'This is the third post\' content'},
-	];
+  posts = [
+    {title: 'First Post', content: 'This is the first post\' content'},
+    {title: 'Second Post', content: 'This is the second post\' content'},
+    {title: 'Third Post', content: 'This is the third post\' content'},
+  ];
 }
