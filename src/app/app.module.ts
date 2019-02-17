cd6278523feec96e5a46a38b7453e46999fb2804
Duc Dao Minh <brokenheart1712@gmail.com>
2019-02-17 19:10:25 +0700

    Creating Post Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './post-list/post-list.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
